86a6e63476a748508a3919b695424a1c85ce1b1b
Szabat195727 <195727@edu.p.lodz.pl>
2017-06-07 18:06:42 +0200

    Add Flight model and factory. Add Lombok

---
+++ src/main/java/Helpers/FlightFactory.java
@@ -0,0 +1,28 @@
+package Helpers;
+
+import Model.Flight;
+import com.fasterxml.jackson.databind.JsonNode;
+
+/**
+ * Created by krystian on 07.06.17.
+ */
+public class FlightFactory {
+
+    public static Flight makeFlight(JsonNode source){
+        Flight f = new Flight();
+
+        f.setId(source.get("id").asText());
+        f.setFlightName(source.get("flightName").asText());
+        f.setFlightDirection(source.get("flightDirection").asText());
+        f.setFlightDestination(source.get("route").get("destinations").asText());
+        f.setScheduleDate(source.get("scheduleDate").asText());
+        f.setScheduleTime(source.get("scheduleTime").asText());
+        f.setMainFlight(source.get("mainFlight").asText());
+        f.setGate(source.get("gate").asText());
+        f.setTerminal(source.get("terminal").asText());
+        f.setFlightNumber(source.get("flightNumber").asInt());
+
+        return f;
+    }
+
+}
---
+++ src/main/java/Model/Flight.java
@@ -0,0 +1,20 @@
+package Model;
+
+import lombok.Data;
+
+/**
+ * Created by krystian on 07.06.17.
+ */
+@Data
+public class Flight {
+    private String id;
+    private String flightName;
+    private String flightDirection;
+    private String flightDestination;
+    private String scheduleDate;
+    private String scheduleTime;
+    private String mainFlight;
+    private String gate;
+    private String terminal;
+    private int flightNumber;
+}
